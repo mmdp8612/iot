ebc371151464a9639aa63be37403028f8428fa1e
mmdp8612 <mmdp8612@gmail.com>
2021-05-30 10:27:56 -0300

    Modificaciones

--- app-iot.js
+++ app-iot.js
@@ -1,4 +1,4 @@
-//version: 3.0.0
+//version: 1.0.0
 
 var mqtt = require('mqtt');
 var express = require('express');
@@ -29,12 +29,8 @@ client.on('error', mqtt_error);
 client.on('message', mqtt_messageReceived);
 client.on('close', mqtt_close);
 
-//client.publish("test", "Hola como estas");
-
 function mqtt_connect(){
-	
 	client.subscribe(Topic, mqtt_suscribe);
-
 }
 
 function mqtt_suscribe(err, granted){
@@ -45,56 +41,33 @@ function mqtt_suscribe(err, granted){
 }
 
 function mqtt_reconnect(err){
-
 	client = mqtt.connect(Broker_URL, options);
-
 }
 
-function mqtt_error(err){
+function mqtt_error(err){}
 
-	
-
-}
-
-function after_publish(){
-	
-	
-	
-}
+function after_publish(){}
 
 function mqtt_messageReceived(topic, message, packet){
-
 	insert_message(topic, message, packet);
-	
-
 }
 
-function mqtt_close(){
-
-	
-
-}
+function mqtt_close(){}
 
 function extract_string(message_str){
-
 	var message_arr = message_str.split(",");
 	return message_arr;
-
 }
 
 var delimiter = ",";
-
 function countInstances(message_str){
-
 	var substrings = message_str.split(delimiter);
 	return substrings.length - 1;
-
 }
 
 function insert_message(topic, message, packet){
 	
 	const objMessage = JSON.parse(String(message));
-	
 	const objTopic = String(topic).split("/");
 	
 	objMessage.topic = {
@@ -104,59 +77,43 @@ function insert_message(topic, message, packet){
 		gateway: objTopic[3]
 	};
 	
-	
-	console.log(objMessage);
-	
 	var url = "mongodb://localhost:27017/";
-
 	MongoClient.connect(url, function(err, db) {
-	  
 	  if (err) throw err;
-	  
-	  var dbo = db.db("mydb");
-	  
+	  const dbo = db.db("mydb");
 	  dbo.collection("iot_devices").insertOne(objMessage, function(err, res) {
 	    if (err) throw err;
 	    console.log("Registro insertado con exito...");
 	    db.close();
 	  });
 	});
-
 }
 
 app.get('/drop', function (req, res){
-	var url = "mongodb://localhost:27017/";
-	
+	const url = "mongodb://localhost:27017/";
 	MongoClient.connect(url, function(err, db) {
 	  if (err) throw err;
-	  var dbo = db.db("mydb");
+	  const dbo = db.db("mydb");
 	  dbo.collection("iot_devices").drop(function(err, delOK) {
 	    if (err) throw err;
 	    if (delOK) console.log("Mensajes Eliminados...");
 	    db.close();
 	  });
 	});
-	
 })
 
 app.get('/data', function(req, res) {
-  
-	var url = "mongodb://localhost:27017/";
-
+	const url = "mongodb://localhost:27017/";
 	MongoClient.connect(url, function(err, db) {
 	  if (err) throw err;
-	  var dbo = db.db("mydb");
+	  const dbo = db.db("mydb");
 	  dbo.collection("iot_devices").find().toArray(function(err, result) {
 	    if (err) throw err;
-	    console.log(result);
-		  
 	    res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(result));
-		  
+            res.end(JSON.stringify(result));  
 	    db.close();
 	  });
 	});
-  
 });
 
 app.listen(8888, function(){
