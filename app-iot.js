625f89296b04330ae6a04f0af6118872634566f3
mmdp8612 <mmdp8612@gmail.com>
2021-06-15 20:33:23 -0300

    modificacion

--- app-iot.js
+++ app-iot.js
@@ -148,11 +148,11 @@ app.get('/:id_cliente/devices', function(req, res) {
 	});
 });
 
-app.get('/transmision', function(req, res) {
+app.get('/transmision/:id_device', function(req, res) {
 	MongoClient.connect(url, function(err, db) {
 	  if (err) throw err;
 	  const dbo = db.db("db_iot");
-	  dbo.collection("iot_messages").find().toArray(function(err, result) {
+	  dbo.collection("iot_messages").find({idDevice: req.params.id_device}).toArray(function(err, result) {
 	    if (err) throw err;
 	    res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result));  
@@ -161,11 +161,11 @@ app.get('/transmision', function(req, res) {
 	});
 });
 
-app.get('/transmision/:id_device', function(req, res) {
+app.get('/transmision', function(req, res) {
 	MongoClient.connect(url, function(err, db) {
 	  if (err) throw err;
 	  const dbo = db.db("db_iot");
-	  dbo.collection("iot_messages").find({IdDevice: req.params.id_device}).toArray(function(err, result) {
+	  dbo.collection("iot_messages").find().toArray(function(err, result) {
 	    if (err) throw err;
 	    res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result));  
